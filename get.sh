8939e0c9dceddacb60570eb35b057896e3f270c0
Santiago M. Mola <santi@mola.io>
2018-05-17 15:42:50 +0200

    get.sh: add support for Appveyor

--- get.sh
+++ get.sh
@@ -5,9 +5,12 @@ arch="amd64"
 os="linux"
 if [[ $TRAVIS_OS_NAME = osx ]]; then
     os="darwin"
+elif [[ ! -z $APPVEYOR ]]; then
+    os="windows"
 fi
 
 tag=$(wget -qSO- --method=HEAD --max-redirect=0 https://github.com/smola/ci-tricks/releases/latest 2>&1 | grep Location: | sed -e 's/.*\///g')
 wget -qO ci-tricks https://github.com/smola/ci-tricks/releases/download/${tag}/ci-tricks_${os}_${arch}
 chmod +x ci-tricks
 exec ./ci-tricks
+
